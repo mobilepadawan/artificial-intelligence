830931a7eb44cac0be26d2fea03afacf98c1fc2f
Fernando <48518539+mobilepadawan@users.noreply.github.com>
2024-05-15 21:59:38 -0300

    Added a new voice and speak project

---
+++ Speech-Synthesis-Voice-Recognition/js/main.js
@@ -0,0 +1,118 @@
+const sound = ["sounds/start.mp3", "sounds/stop.mp3"]
+const audio = new Audio()
+const paragraphInstruction = document.querySelector("input#paragraphInstruction")
+const buttonTalk = document.querySelector("button.user-send-button")
+let isTalking = false
+
+// VOICE RECOGNITION
+window.speechRecognition = window.speechRecognition || window.webkitSpeechRecognition
+const recognition = new speechRecognition() || new webkitSpeechRecognition()
+
+recognition.interimResults = true
+recognition.continous = true
+recognition.lang = "en-US"
+
+Speakit.utteranceRate = 1
+Speakit.utterancePitch = 1
+
+function animateTalking() {
+    const voiceCircle = document.querySelector("div.spinner")
+    voiceCircle.style.animation = "spinning 0.7s linear infinite, hue 0.6s ease-in-out infinite"
+}
+
+function stopAnimateTalking() {
+    const voiceCircle = document.querySelector("div.spinner")
+    voiceCircle.style.animation = ""
+}
+
+function animateButtonStyle() {
+    buttonTalk.style.fontSize = "26px"
+    buttonTalk.style.backgroundColor = "#3b3a3a"
+    buttonTalk.style.border = "2px solid red"
+}
+
+function stopAnimateButtonStyle() {
+    buttonTalk.style.border = "none"
+    buttonTalk.style.backgroundColor = "rgb(109, 104, 104)"
+    buttonTalk.style.fontSize = "32px"
+}
+
+// MAIN FUNCTION
+recognition.start()
+
+// EVENTS
+buttonTalk.addEventListener("click", ()=> {
+    if (isTalking) {
+        audio.src = sound[1]
+        audio.play()
+        stopAnimateTalking()
+        stopAnimateButtonStyle()
+        isTalking = false
+    } else {
+        audio.src = sound[0]
+        audio.play()
+        animateTalking()
+        animateButtonStyle()
+        isTalking = true
+    }
+})
+
+recognition.addEventListener("start", ()=> { 
+    console.log("✅ Voice recognition is ready to hear you.")
+})
+
+recognition.addEventListener("soundstart",()=> {
+    // animateButtonStyle()
+    animateTalking()
+})
+recognition.addEventListener("soundend",()=> {
+    // stopAnimateButtonStyle()
+    stopAnimateTalking()
+})
+
+recognition.addEventListener("result", (e)=> {
+    console.log(e.results[0])
+    console.log(e.results[0].transcript)
+    const text = Array.from(e.results).map((result)=> result[0].transcript)
+                                      .join('')
+                                      console.log(text)
+
+    if (e.results[0].isFinal) {
+        console.log(text)
+        // paragraphInstruction.value += text + " "
+
+        if (text.toLowerCase().includes("what is your name")) {
+            Speakit.readText("I am JARVIS. A computer system developed by the best programmer ever: FERNANDO.", "en-GB")
+        }
+
+        if (text.toLowerCase().includes("are you still working for tony")) {
+            Speakit.readText("Not now. I left my working position for the IRON MAN. I am a professional freelancer already.", "en-GB")
+        }
+
+        if (text.toLowerCase().includes("hello jarvis") ) {
+            Speakit.readText("Hello to you! What is your name?", "en-GB")
+        }
+
+        if (text.toLowerCase().includes("my name is")) {
+            let name = text.split(" ")
+            Speakit.readText(`Hello, ${name[name.length - 1]}! Nice to meet you.`, "en-GB")
+        }
+
+        if (text.toLowerCase().includes("open youtube")) {
+            Speakit.readText("Opening Youtube...", "en-GB")
+            setTimeout(() => {
+                let anchor = document.createElement("a")
+                anchor.target  = "_blank"
+                anchor.href = "https://www.youtube.com/"
+                anchor.click()
+            }, 1000)
+        }
+
+        if (text.toLowerCase().includes("goodbye")) {
+            Speakit.readText("Goodbye Sir. See you soon. Have a good night!", "en-GB")
+
+        }
+    }
+})
+
+recognition.addEventListener('end', ()=> recognition.start() )
---
+++ Speech-Synthesis-Voice-Recognition/readme.md
@@ -0,0 +1,17 @@
+# Voice recognition and Speech Synthesis
+
+I built an App where there is implemented voice recognition native also Speech Synthesis. This last one is working through Speakit-JS library.
+
+## Malfunctioning
+
+Voice Recognition is not working in the following web browsers:
+* Safari browser
+* Firefox browser
+* Microsoft Edge for MacOS
+
+This code is just for fun. In the coming weeks we will moving it to a serious web application.
+
+## Animation
+The animation integrated it works combining classic CSS animation properties over a DIV element and controlling the CSS animation start and end from JavaScript.
+
+This animation is integrated with the `soundstart` and `soundend` events propper of `SpeechRecognition` object.
