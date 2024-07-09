b87715d69ab46eb93cd9242a93fc4d38b08c5a8f
Fernando <48518539+mobilepadawan@users.noreply.github.com>
2024-07-09 20:33:51 -0300

    Fine tuning of CSS and Vite functions

--- Chatbot/js/main.js
+++ Chatbot/js/main.js
@@ -4,10 +4,8 @@ const chatContainer = document.querySelector("div.chat-container")
 const btnEnviar = document.querySelector("button#btnEnviar")
 const txtMensaje = document.querySelector("input#txtMensaje")
 
-
 btnEnviar.addEventListener("click", ()=> {
     const messageText = txtMensaje.value.trim()
-
     if (messageText) {
         chatContainer.innerHTML += returnBubbleChatClient(messageText)
         chatContainer.innerHTML += returnSpaceInChat()
