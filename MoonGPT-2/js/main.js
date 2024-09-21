1d64238d99a8b7c1e61ff2e0b9c59ec5c6dc74d0
Fernando Luna <48518539+mobilepadawan@users.noreply.github.com>
2024-09-21 14:16:47 -0300

    Integrating the model selection & auto-scrolling

--- MoonGPT-2/js/main.js
+++ MoonGPT-2/js/main.js
@@ -1,16 +1,29 @@
 import { modelsArray, D, returnChatMessageAssistant, 
-         returnChatMessageUser, } from "./elements.js"
-import { playAudioFile } from "./audio.js"
-import { getAnswerFromGemini } from "./models.js"
-import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js"
+         returnChatMessageUser, } from './elements.js'
+import { playAudioFile } from './audio.js'
+import { getAnswerFromGemini } from './models.js'
+import { marked } from 'https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js'
 
+// ENLACES DOM
 const languageSelect = D('select#language-select')
 const settingsIcon = D('span#settings-icon')
 const chatContainer = D('div.chat-container')
 const textArea = D('textArea#chat-input')
 const formQuestion = D('form')
 
+// VARIABLES
+let selectedModel = ''
+
 // FUNCIONALIDADES
+
+function returnNoModelSelected() {
+    chatContainer.innerHTML = returnChatMessageAssistant('Lo siento, pero no hay modelos para interactuar. 😕 \n Selecciona un Modelo desde el extremo superior izquierdo.')
+}
+
+function scrollToBottom() {
+    document.querySelector('main').scrollTop = chatContainer.scrollHeight
+}
+
 function initPlatform() {
     chatContainer.innerHTML = ''
 
@@ -19,7 +32,7 @@ function initPlatform() {
                     languageSelect.innerHTML += `<option value="${model}">${model}</option>`)
         chatContainer.innerHTML = returnChatMessageAssistant('👋🏼 ¡Te damos la Bienvenida! <strong>Selecciona un Modelo</strong> para comenzar a interactuar.')
     } else {
-        chatContainer.innerHTML = returnChatMessageAssistant('Lo siento, pero no hay modelos para interactuar. 😕')
+        returnNoModelSelected()
     }
 }
 
@@ -32,26 +45,58 @@ formQuestion.addEventListener('submit', async (e)=> {
     e.preventDefault()
     const question = textArea.value.trim()
 
-    if (question !== '') {
-        chatContainer.innerHTML += returnChatMessageUser(question)
-        playAudioFile('audio/msg_send.mp3')
-        textArea.value = ''
-        getAnswerFromGemini(question).then((response )=> {
-            const answer = marked.parse(response)
-            chatContainer.innerHTML += returnChatMessageAssistant(answer)
-            playAudioFile('audio/msg_recv.mp3')    
-        })
+    if (!selectedModel) {
+        returnNoModelSelected()
+    } else {
+        if (question !== '') {
+            chatContainer.innerHTML += returnChatMessageUser(question)
+            playAudioFile('audio/msg_send.mp3')
+            scrollToBottom()
+            textArea.value = ''
+
+            if (selectedModel === 'Gemini') {
+                const response = await getAnswerFromGemini(question)
+                const answer = marked.parse(response)
+                chatContainer.innerHTML += returnChatMessageAssistant(answer)
+                playAudioFile('audio/msg_recv.mp3')
+                scrollToBottom()
+
+                // getAnswerFromGemini(question).then((response )=> {
+                //     const answer = marked.parse(response)
+                //     chatContainer.innerHTML += returnChatMessageAssistant(answer)
+                //     playAudioFile('audio/msg_recv.mp3')
+                //     scrollToBottom()
+                // })
+            }
+            if (selectedModel === 'Chat GPT') {
+                setTimeout(() => {
+                    const chatNotification = `Todavía no estoy activo 😔, por lo cual te invito a que selecciones otro Modelo entre las opciones disponibles.`
+                    chatContainer.innerHTML += returnChatMessageAssistant(chatNotification)
+                    playAudioFile('audio/msg_recv.mp3')
+                    scrollToBottom()
+                }, 1000)
+            }
+            return 
+        }
+    }
+})
+
+textArea.addEventListener('keydown', (e)=> {
+    if (e.key === 'Enter') {
+        e.preventDefault()
+        formQuestion.dispatchEvent(new Event('submit'))
     }
 })
 
 languageSelect.addEventListener('change', ()=> {
-    const assistantSelected = languageSelect.value
-
-    if (assistantSelected !== 'Selecciona un Modelo...') {
+    selectedModel = languageSelect.value
+    console.log(selectedModel)
+    if (selectedModel !== 'Selecciona un Modelo...') {
         setTimeout(() => {
-            const modeloSelected = `Conversarás con el Modelo 🤖 <strong>${assistantSelected}</strong>`
-            chatContainer.innerHTML += returnChatMessageAssistant(modeloSelected)
+            const chatNotification = `Conversarás con el Modelo 🤖 <strong>${selectedModel}</strong>`
+            chatContainer.innerHTML += returnChatMessageAssistant(chatNotification)
             playAudioFile('audio/msg_recv.mp3')
+            scrollToBottom()
         }, 600)
     }
 })
